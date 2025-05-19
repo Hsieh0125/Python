7b18862ffbfe17ac86065cf7e0da1d9160571390
114514 <xieyu9023@gmail.com>
2025-05-19 08:38:33 +0800

    Update f579題解.py

--- Zerojudge 練習/f579題解.py
+++ Zerojudge 練習/f579題解.py
@@ -1,23 +1,15 @@
-a,b=map(int, input().split())
-n=int(input())
-
+a,b=map(int,input().split())
+c=int(input())
 count=0
 
-
-for i in range(n):
-        item=list(map(int, input().split()))
-        car = []
-        num = [0]*101
-
-        for j in item:
-                if j>0:
-                        car.append(j)
-                elif j<0:
-                        if -j in car:
-                                car.remove(-j)
-                elif j==0:
-                        break
-
-        if a in car and b in car:
-                count = count+1
+for i in range(c):
+    car=[]
+    e=list(map(int,input().split()))
+    for j in e:
+        if j>0:
+            car.append(j)
+        elif j<0:
+            car.remove(-j) #-(-j)=j
+    if a in car and b in car:
+        count+=1
 print(count)
