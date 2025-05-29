ca0d7bb65ac03de091c2b8fb02f0d239784b870f
114514 <xieyu9023@gmail.com>
2025-05-29 09:32:35 +0800

    Update j605題解.py

--- Zerojudge 練習/j605題解.py
+++ Zerojudge 練習/j605題解.py
@@ -1,21 +1,20 @@
-a=int(input())
-t=[]
-s=[]
-for i in range(a):
-    ti,sc=map(int, input().split())
-    t.append(ti)
-    s.append(sc)
-err=0
-for i in range(1,len(s)):
-    if  s[i]==-1:
-        err+=1
-msc=max(s)-a-(err*2)
-sta=s[0]
-stt=t[0]
-for j in  range(1,a):
-    if s[j]>sta:
-        sta=s[j]
-        stt=t[j]
-if msc<0:
-    msc=0
-print(msc,stt)
+n=int(input())
+ti=[]
+sc=[]
+for i in range(n):
+    t,s=map(int,input().split())
+    ti.append(t)
+    sc.append(s)
+maxgrade=sc[0]
+time=ti[0]
+wr=0
+for i in range(1,n):
+    if sc[i]>maxgrade:
+        maxgrade=sc[i]
+        time=ti[i]
+    elif sc[i]==-1:
+        wr+=1
+ans=max(sc)-n-wr*2
+if ans<0:
+    ans=0
+print(ans,time)
