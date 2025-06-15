f60b0ce5a7270d2a88e4e64c3c570975e45bc161
114514 <xieyu9023@gmail.com>
2025-06-15 08:46:35 +0800

    Update g595題解.py

--- Zerojudge 練習/g595題解.py
+++ Zerojudge 練習/g595題解.py
@@ -1,13 +1,11 @@
-a=int(input())
-b=list(map(int,input().split()))
-count=0
-for i in range(a):
-    if b[i]==0:
-        if i==0:
-            count+=b[i+1]
-        elif i==a-1:
-            count+=b[i-1]
-        else:
-            pl=min(b[i-1],b[i+1])
-            count+=pl
-print(count)
+n=int(input())
+a=list(map(int,input().split()))
+fix=0
+if a[0]==0:
+    fix+=a[1]
+if a[-1]==0:
+    fix+=a[-2]
+for i in range(1,len(a)-1):
+    if a[i]==0:
+        fix+=min(a[i-1],a[i+1])
+print(fix)
